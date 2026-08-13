6adcb2198544d6e9dcb0c190ef571a2112d4f67a
Tim Schilling <schillingt@better-simple.com>
2026-08-13 09:46:13 -0500

    Propose adjusting the approach to reduce the amount of copied code.
    - Renames the sphinx test directory to allow importing of sphinx
    - Creates a base TestCase class that manages setting up docs imports
    - Creates an API method for subclasses to import specific modules
    - Skips sphinx tests when sphinx isn't installed via skipIf

--- tests/sphinx_tests/test_djangodocs.py
+++ tests/sphinx_tests/test_djangodocs.py
@@ -1,44 +1,18 @@
-import pathlib
-import sys
 from types import SimpleNamespace
-from unittest import SkipTest
 
-from django.test import SimpleTestCase
+from sphinx_tests.tests import SimpleSphinxTestCase
 
 # The import must happen at the end of setUpClass, so it can't be imported at
 # the top of the file.
 djangodocs = None
 
 
-class DjangoDocsTests(SimpleTestCase):
+class DjangoDocsTests(SimpleSphinxTestCase):
     @classmethod
-    def setUpClass(cls):
-        # The file implementing the code under test is in the docs folder and
-        # is not part of the Django package. This means it cannot be imported
-        # through standard means. Include its parent in the pythonpath for the
-        # duration of the tests to allow the code to be imported.
-        cls.ext_path = str((pathlib.Path(__file__).parents[2] / "docs/_ext").resolve())
-        sys.path.insert(0, cls.ext_path)
-        cls.addClassCleanup(sys.path.remove, cls.ext_path)
-        cls.addClassCleanup(sys.modules.pop, "djangodocs", None)
-        cls.addClassCleanup(sys.modules.pop, "github_links", None)
-
-        # The test package is named "sphinx", which shadows the Sphinx package
-        # when the extension is imported.
-        tests_path = str(pathlib.Path(__file__).parents[1].resolve())
-        sys.path.remove(tests_path)
-        cls.addClassCleanup(sys.path.insert, 0, tests_path)
-        test_package = sys.modules.pop("sphinx", None)
-        if test_package is not None:
-            cls.addClassCleanup(sys.modules.__setitem__, "sphinx", test_package)
-
+    def docs_module_import(cls):
+        """Override this method to allow importing code in the docs folder."""
         # Linters/IDEs may not be able to detect this as a valid import.
-        try:
-            import djangodocs as _djangodocs
-        except ModuleNotFoundError as exc:
-            if exc.name == "sphinx":
-                raise SkipTest("Sphinx is not installed.") from exc
-            raise
+        import djangodocs as _djangodocs
 
         global djangodocs
         djangodocs = _djangodocs
